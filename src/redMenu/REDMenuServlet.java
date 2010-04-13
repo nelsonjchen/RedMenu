ebac94c8f12185c2df5ac7e093d209ce5039680b
Johan Henkens <jomanscool2@yahoo.com>
2010-04-12 23:37:21 -0700

    First commit

---
+++ src/redMenu/REDMenuServlet.java
@@ -0,0 +1,13 @@
+package redMenu;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class REDMenuServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
