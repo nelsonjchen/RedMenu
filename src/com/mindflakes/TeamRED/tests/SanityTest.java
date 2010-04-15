8f0f2427c9784d8d8915a203da7f5f922c7501d4
Nelson Chen <crazysim@gmail.com>
2010-04-15 13:13:23 -0700

    just a simple sanity test added along with package and so on

---
+++ src/com/mindflakes/TeamRED/tests/SanityTest.java
@@ -0,0 +1,13 @@
+package com.mindflakes.TeamRED.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SanityTest {
+
+	@Test
+	public void checkEqual() {
+		assertEquals("One Equals One",1,1);
+	}
+}
