018d7a10d116e50acb8be2e2c3740bd2302dd558
Nelson Chen <crazysim@gmail.com>
2010-04-17 20:08:37 -0700

    moved testing jar

---
+++ src/com/mindflakes/TeamRED/tests/LightSwitchStoreTest.java
@@ -0,0 +1,29 @@
+package com.mindflakes.TeamRED.tests;
+
+import static junit.framework.Assert.assertEquals;
+
+import static org.junit.Assert.assertEquals;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.vercer.engine.persist.annotation.AnnotationObjectDatastore;
+
+import org.junit.Test;
+
+
+public class LightSwitchStoreTest extends LocalDatastoreTestCase {
+	
+	private AnnotationObjectDatastore datastore;
+
+	@Override
+	public void setUp()
+	{
+		super.setUp();
+		DatastoreService service = DatastoreServiceFactory.getDatastoreService();
+		datastore = new AnnotationObjectDatastore(service);
+	}
+
+	@Test
+	public void checkEqual() {
+		assertEquals("One Equals One",1,1);
+	}
+}
