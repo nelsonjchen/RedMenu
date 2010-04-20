7ba6c40410b82f4e3e1cc075aad7437bd9da3ac6
Nelson Chen <crazysim@gmail.com>
2010-04-20 13:48:04 -0700

    added test for searching for fooditems; fails but we'll get it working later

--- src/com/mindflakes/TeamRED/tests/MealMenuTest.java
+++ src/com/mindflakes/TeamRED/tests/MealMenuTest.java
@@ -1,15 +1,16 @@
 package com.mindflakes.TeamRED.tests;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.Iterator;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.mindflakes.TeamRED.UCSBScrape.TestMain;
+import com.mindflakes.TeamRED.menuClasses.FoodItem;
 import com.mindflakes.TeamRED.menuClasses.MealMenu;
 import com.vercer.engine.persist.annotation.AnnotationObjectDatastore;
+import static com.google.appengine.api.datastore.Query.FilterOperator.*;
 
 import org.junit.Test;
 
@@ -28,6 +29,11 @@ public class MealMenuTest extends LocalDatastoreTestCase {
 		datastore = new AnnotationObjectDatastore(service);
 	}
 	
+	private void storeMenu() {
+		mealmenu = TestMain.createTestMenu();
+		datastore.store().instance(mealmenu).returnKeyNow();
+	}
+	
 	@Test
 	public void checkEqual() {
 		assertEquals("One Equals One",1,1);
@@ -44,10 +50,21 @@ public class MealMenuTest extends LocalDatastoreTestCase {
 		mealmenu = TestMain.createTestMenu();
 		datastore.store().instance(mealmenu).returnKeyNow();
 		Iterator<MealMenu> retrieved_menus = datastore.find().type(MealMenu.class)
-		.addFilter("commonsName", FilterOperator.EQUAL, "Carrillo")
+		.addFilter("commonsName", EQUAL, "Carrillo")
 		.returnResultsNow();
 		MealMenu retrieve = retrieved_menus.next();
 		assertEquals("Name", "Carrillo", retrieve.getCommonsName());
 	}
 
-}
+	@Test
+	
+	public void findCommonByCheeze() {
+		storeMenu();
+		Iterator<FoodItem> retreived_fooditems = datastore.find()
+		.type(FoodItem.class)
+		.addFilter("Cheeze", IN, "name").returnResultsNow();
+		FoodItem item = retreived_fooditems.next();
+		assertTrue("Item is Cheezy", item.getName().contains("Cheeze"));
+		
+	}
+}	
