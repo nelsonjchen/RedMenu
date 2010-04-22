f44e412764ea62d6de8321f54814085e69941e1c
Nelson Chen <crazysim@gmail.com>
2010-04-22 13:14:38 -0700

    added a test for the common name

--- src/com/mindflakes/TeamRED/tests/UCSBMenuScraperTest.java
+++ src/com/mindflakes/TeamRED/tests/UCSBMenuScraperTest.java
@@ -1,6 +1,29 @@
 package com.mindflakes.TeamRED.tests;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import com.mindflakes.TeamRED.UCSBScrape.UCSBMenuScraper;
+import com.mindflakes.TeamRED.menuClasses.MealMenu;
+
 
 public class UCSBMenuScraperTest {
-
+	ArrayList<MealMenu> menus;
+	
+	public void setUp() {
+//		Carrillo
+        
+	}
+	
+	@Test
+	public void checkName() {
+		new UCSBMenuScraper("docs/viewer.xml", 1);
+        menus = UCSBMenuScraper.getMealMenu();
+		assertEquals("Common Name Matches","Carrillo",menus.get(0).getCommonsName());
+	}
 }
