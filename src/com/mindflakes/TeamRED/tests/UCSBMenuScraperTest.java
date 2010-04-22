5d78040d7e6aaf88949639ed2a78d412a59228fe
Nelson Chen <crazysim@gmail.com>
2010-04-22 13:33:21 -0700

    added more tests for meal name and venue

--- src/com/mindflakes/TeamRED/tests/UCSBMenuScraperTest.java
+++ src/com/mindflakes/TeamRED/tests/UCSBMenuScraperTest.java
@@ -27,7 +27,17 @@ public class UCSBMenuScraperTest {
 	public void checkName() {
 		assertEquals("Common Name Matches","Ortega",menus.get(0).getCommonsName());
 	}
+	
+	@Test
+	public void checkMealName() {
+		assertEquals("Meal Name Matches","Breakfast",menus.get(0).getMealName());
+	}
 
+	@Test
+	public void ensureVenuesPresent() {
+		assertTrue("Venues are present", menus.get(0).getVenues().size() > 0);
+	}
+	
 	@Test
 	public void checkStartTimeForBreakfast() {
 		DateTime common_open = new DateTime(2010, 4, 19, 7, 15, 0, 0);
@@ -41,4 +51,5 @@ public class UCSBMenuScraperTest {
 		assertEquals("End time is correct", common_close.getMillis(),
 				menus.get(0).getMealInterval().getEndMillis());
 	}
+	
 }
