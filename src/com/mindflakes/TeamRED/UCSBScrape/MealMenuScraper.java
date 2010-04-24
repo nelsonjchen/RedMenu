148a1cfc06409ca2595d46a63363a0320941e936
Nelson Chen <crazysim@gmail.com>
2010-04-24 03:46:26 -0700

    new scraper interface

--- src/com/mindflakes/TeamRED/UCSBScrape/MealMenuScraper.java
+++ src/com/mindflakes/TeamRED/UCSBScrape/MealMenuScraper.java
@@ -1,5 +1,10 @@
 package com.mindflakes.TeamRED.UCSBScrape;
 
-public interface MealMenuScraper {
+import java.io.InputStream;
+import java.util.ArrayList;
 
+import com.mindflakes.TeamRED.menuClasses.MealMenu;
+
+public interface MealMenuScraper {
+	ArrayList<MealMenu> parse(InputStream s);
 }
--- src/com/mindflakes/TeamRED/UCSBScrape/UCSBMealMenuScraper.java
+++ src/com/mindflakes/TeamRED/UCSBScrape/UCSBMealMenuScraper.java
@@ -1,5 +1,19 @@
 package com.mindflakes.TeamRED.UCSBScrape;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import com.mindflakes.TeamRED.menuClasses.MealMenu;
+
 public class UCSBMealMenuScraper implements MealMenuScraper {
+    private static final Logger log = Logger.getLogger(UCSBMealMenuScraper.class.getName());
+
+	@Override
+	public ArrayList<MealMenu> parse(InputStream s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	
 }
