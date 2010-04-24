8ddcf916d1f72d2cb622bbc273ebef083ce8adc7
Nelson Chen <crazysim@gmail.com>
2010-04-24 13:00:55 -0700

    figured out why it was nullpointerexception

--- src/com/mindflakes/TeamRED/tests/DOMParsingPlayTestWithCommonXML.java
+++ src/com/mindflakes/TeamRED/tests/DOMParsingPlayTestWithCommonXML.java
@@ -9,9 +9,11 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 public class DOMParsingPlayTestWithCommonXML {
-	private Document doc;
+	private static Document doc;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -19,11 +21,11 @@ public class DOMParsingPlayTestWithCommonXML {
             DocumentBuilderFactory.newInstance();
 
         // Turn off validation, and turn off namespaces
-        factory.setValidating(false);
-        factory.setNamespaceAware(false);
+//        factory.setValidating(false);
+//        factory.setNamespaceAware(false);
 
         DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.parse(
+        doc = builder.parse(
         		new File("docs/specimens/ortega/-xxxx-tthom-042010-110140.xml"));
 	}
 	
@@ -34,7 +36,10 @@ public class DOMParsingPlayTestWithCommonXML {
 	
 	@Test
 	public void showCommonName() {
-		
+
+		doc.toString();
+		NodeList nodes = doc.getElementsByTagName("*");
+		System.out.println(nodes.getLength());
 	}
 	
 	
