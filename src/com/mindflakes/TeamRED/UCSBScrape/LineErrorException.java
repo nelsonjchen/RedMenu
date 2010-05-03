1095afe6da6a52c2d955956ef7eef514336b7782
Johan Henkens <Johan Henkens@169.231.8.77>
2010-05-02 22:33:16 -0700

    Documentation/UML changes.

--- src/com/mindflakes/TeamRED/UCSBScrape/LineErrorException.java
+++ src/com/mindflakes/TeamRED/UCSBScrape/LineErrorException.java
@@ -1,13 +1,27 @@
 package com.mindflakes.TeamRED.UCSBScrape;
 
+/**
+ * @author Johan Henkens
+ *
+ */
 public class LineErrorException extends RuntimeException {
 
+	/**
+	 * Constructs a new LineErrorException with the specified description.
+	 * @param throwError String containing description of the LineErrorException that occurred
+	 */
 	public LineErrorException(String throwError){
 		super(throwError);
 	}
 	
-	public LineErrorException(String throwError, Throwable t){
-		super(throwError,t);
+	
+	/**
+	 * Constructs a new LineErrorException with the specified description and cause.
+	 * @param throwError description of the LineErrorException that occurred
+	 * @param cause the cause of the Exception
+	 */
+	public LineErrorException(String throwError, Throwable cause){
+		super(throwError,cause);
 	}
 	
 	
