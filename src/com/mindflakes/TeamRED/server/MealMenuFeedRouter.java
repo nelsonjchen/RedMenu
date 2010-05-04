4bb748d019b38e9e34a3f3344bab5d65fed00365
Nelson Chen <crazysim@gmail.com>
2010-05-04 13:02:28 -0700

    testing prelim custom urls

---
+++ src/com/mindflakes/TeamRED/server/MealMenuFeedRouter.java
@@ -0,0 +1,28 @@
+package com.mindflakes.TeamRED.server;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MealMenuFeedRouter extends HttpServlet{
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) 
+	throws IOException{
+	final String path = req.getPathInfo();
+	Pattern MEAL_COMMON_PATTERN = Pattern.compile("/feed/(.*?)");
+	Matcher m = null;
+	
+	m = MEAL_COMMON_PATTERN.matcher(path);
+	if (m.matches()) {
+		final String common = m.group(1);
+		resp.getWriter().print("Matched common: " + common);
+	} else {
+		resp.getWriter().print("No common matched.");
+	}
+	
+	}
+}
---
+++ src/com/mindflakes/TeamRED/server/MealMenuUpcomingFeedServlet.java
@@ -0,0 +1,17 @@
+package com.mindflakes.TeamRED.server;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class MealMenuUpcomingFeedServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+    throws IOException {
+		final String pathinfo = req.getPathInfo();
+		
+		
+	}
+}
