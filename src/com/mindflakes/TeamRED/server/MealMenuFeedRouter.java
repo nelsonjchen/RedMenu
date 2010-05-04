7afb186fd087ad3ebee2392b3b1fa9f2f7bd1e89
Nelson Chen <crazysim@gmail.com>
2010-05-04 13:11:52 -0700

    trying to get working still

--- src/com/mindflakes/TeamRED/server/MealMenuFeedRouter.java
+++ src/com/mindflakes/TeamRED/server/MealMenuFeedRouter.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("serial")
 public class MealMenuFeedRouter extends HttpServlet{
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) 
